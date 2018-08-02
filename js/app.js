7623e9c027cc044e6670910ae89dcc07403e7931
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-08-02 13:05:31 +0200

    Css

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+var prevBtn = document.querySelector("#prevPicture");
+var nextBtn = document.querySelector("#nextPicture");
+
+
+
+
+
+
+});
