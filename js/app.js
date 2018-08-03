640845147cb7b21d2e81373582c44316e3cc8271
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-08-03 14:07:05 +0200

    Pricing and footer section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){
-var prevBtn = document.querySelector(".slider__button--prev");
-var nextBtn = document.querySelector(".slider__button--next");
+document.addEventListener("DOMContentLoaded", function() {
+  var prevBtn = document.querySelector(".slider__button--prev");
+  var nextBtn = document.querySelector(".slider__button--next");
 
 
 
